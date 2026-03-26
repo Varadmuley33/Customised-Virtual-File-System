d8b53444630fc8ef4fec1888939d1c39127f6f38
Varad Muley <varad.muley408@gmail.com>
2026-03-26 14:47:46 +0530

    Add files via upload

---
+++ CVFSmain.cpp
@@ -0,0 +1,211 @@
+#include "CVFS.h"
+
+int main()
+{
+    char str[80] = {'\0'};
+    char Command[5][20] = {{'\0'}};
+    char InputBuffer[MAXFILESIZE] = {'\0'};
+
+    char *EmptyBuffer = NULL;
+
+    int iCount = 0;
+    int iRet = 0;
+
+    StartAuxillaryDataInitilisation();
+
+    printf("-----------------------------------------------\n");
+    printf("-----------CVFS started succesfully -----------\n");
+    printf("-----------------------------------------------\n");
+    
+    // Infinite Listening Shell
+    while(1)
+    {
+        fflush(stdin);
+
+        strcpy(str,"");
+
+        printf("\nCVFS : > ");
+        fgets(str,sizeof(str),stdin);
+        
+        iCount = sscanf(str,"%s %s %s %s %s",Command[0],Command[1],Command[2],Command[3], Command[4]);
+
+        fflush(stdin);
+
+        if(iCount == 1)
+        {
+            // CVFS : > exit
+            if(strcmp("exit",Command[0]) == 0)
+            {
+                printf("Thank you for using CVFS\n");
+                printf("Deallocating all the allocated resources\n");
+
+                break;
+            }
+            // CVFS : > ls
+            else if(strcmp("ls",Command[0]) == 0)
+            {
+                LsFile();
+            }
+            // CVFS : > help
+            else if(strcmp("help",Command[0]) == 0)
+            {
+                DisplayHelp();
+            }
+            // CVFS : > clear
+            else if(strcmp("clear",Command[0]) == 0)
+            {
+                #ifdef _WIN32
+                    system("cls");
+                #else
+                    system("clear");
+                #endif
+            }
+        } // End of else if 1
+        else if(iCount == 2)
+        {
+            // CVFS : > man ls
+            if(strcmp("man",Command[0]) == 0)
+            {
+                ManPageDisplay(Command[1]);
+            }
+            // CVFS : > unlink Demo.txt
+            else if(strcmp("unlink",Command[0]) == 0)
+            {
+                iRet = UnlinkFile(Command[1]);
+            
+                if(iRet == ERR_INVALID_PARAMETER)
+                {
+                    printf("Error : Invalid parameter\n");
+                }
+
+                if(iRet == ERR_FILE_NOT_EXIST)
+                {
+                    printf("Error : Unable to delete as there is no such file");
+                }
+
+                if(iRet == EXECUTE_SUCCESS)
+                {
+                    printf("File gets succesfully deleted\n");
+                }
+            }
+            // CVFS : > stat Demo.txt
+            else if(strcmp("stat",Command[0]) == 0)
+            {
+                StatFile(Command[1]);
+            }
+            // CVFS : > write 2
+            else if(strcmp("write",Command[0]) == 0)
+            {
+                printf("Enter the data that you want to write : \n");
+                fgets(InputBuffer,MAXFILESIZE,stdin);
+
+                iRet = WriteFile(atoi(Command[1]), InputBuffer, strlen(InputBuffer)-1);
+            
+                if(iRet == ERR_INVALID_PARAMETER)
+                {
+                    printf("Error : Invalid parameters \n");
+                }
+                else if(iRet == ERR_FILE_NOT_EXIST)
+                {
+                    printf("Error : There is no such file\n");
+                }
+                else if(iRet == ERR_PERMISSION_DENIED)
+                {
+                    printf("Error : Unable to write as there is no permission\n");
+                }
+                else if(iRet == ERR_INSUFFICIENT_SPACE)
+                {
+                    printf("Error : Unable to write as there is no space\n");
+                }
+                else
+                {
+                    printf("%d bytes gets succesfully written\n",iRet);
+                }
+            }
+            else
+            {
+                printf("There is no such command\n");
+            }
+        } // End of else if 2
+        else if(iCount == 3)
+        {
+            // CVFS : > creat Ganesh.txt 3
+            if(strcmp("creat",Command[0]) == 0)
+            {
+                iRet = CreateFile(Command[1],atoi(Command[2]));
+
+                if(iRet == ERR_INVALID_PARAMETER)
+                {
+                    printf("Error : Unable to create the file as parameters are invalid\n");
+                    printf("Please refer man page\n");
+                }
+
+                if(iRet == ERR_NO_INODES)
+                {
+                    printf("Error : Unable to create file as there is no inode\n");
+                }
+
+                if(iRet == ERR_FILE_ALREADY_EXIST)
+                {
+                    printf("Error : Unable to create file because the file is already present\n");
+                }
+
+                if(iRet == ERR_MAX_FILES_OPEN)
+                {
+                    printf("Error : Unable to create file\n");
+                    printf("Max opened files limit reached\n");
+                }
+
+                printf("File gets succesfully created with FD %d\n",iRet);
+            } 
+            // CVFS : > read 3 10
+            else if(strcmp("read",Command[0]) == 0)
+            {
+                int fd = atoi(Command[1]);   
+
+                uareaobj.UFDT[fd]->ReadOffset = 0;  
+
+                EmptyBuffer = (char *)malloc(atoi(Command[2] + 1));
+
+                iRet = ReadFile(atoi(Command[1]), EmptyBuffer, atoi(Command[2]));
+
+                if(iRet == ERR_INVALID_PARAMETER)
+                {
+                    printf("Error : Invalid parameter\n");
+                }
+                else if(iRet == ERR_FILE_NOT_EXIST)
+                {
+                    printf("Error : File not exist\n");
+                }
+                else if(iRet == ERR_PERMISSION_DENIED)
+                {
+                    printf("Error : Permission denied\n");
+                }
+                else if(iRet == ERR_INSUFFICIENT_DATA)
+                {
+                    printf("Error : Insufficient data\n");
+                }
+                else
+                {
+                    EmptyBuffer[iRet] = '\0';
+
+                    printf("Read operation is succesful\n");
+                    printf("Data from file is : %s\n",EmptyBuffer);
+
+                    free(EmptyBuffer);
+                }
+            }
+            else
+            {
+                printf("There is no such command\n");
+            }
+        } // End of else if 3
+        else
+        {
+            printf("Command not found\n");
+            printf("Please refer help option to get more information\n");
+        } // End of else
+    } // End of while
+
+    return 0;
+} // End of main
